cf0e66ac54d1be5d4954b2093e6ab0f0f4a41bed
armiiiiiin <forutanmohammad@gmail.com>
2026-09-21 11:00:18 +0330

    Add CI/CD pipeline and minor text update

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Documentation - hi mohammadddddddddd
           </a>
         </div>
       </main>
